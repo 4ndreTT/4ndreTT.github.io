c1d220ff5fd3823e3592ee4aa0200b96c03e1857
4ndreTT <andreTT.btw@gmail.com>
2023-01-12 19:57:25 -0500

    bad test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "TCP bind shellcode",
+        "excerpt":"A bind shellcode listens on a socket, waiting for a connection to be made to the server then executes arbitrary code, typically spawning shell for the connecting user. This post demonstrates a simple TCP bind shellcode that executes a shell. The shellcode does the following: Creates a socket Binds the...","categories": ["slae","infosec"],
+        "tags": ["slae","assembly","tcp bind shellcode"],
+        "url": "https://4ndrett.github.io/tcp-bind-shellcode/",
+        "teaser":"https://4ndrett.github.io/assets/images/slae32.png"},{
         "title": "Bandit - Over The Wire",
         "excerpt":"Bandit es uno de los wargames disponibles en OverTheWire, el cual busca ayudar en la mejora de las habilidades de línea de comandos o terminal y la comprensión de la seguridad informática básica en Linux. El jugador debe resolver una serie de desafíos con la finalidad de obtener una “flag”...","categories": ["overthewire","infosec"],
         "tags": ["linux","bash","terminal"],
